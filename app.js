881c4be76a30ed5ae295598310617a9ef26b36e7
alessandrob17 <a.baldipardi@gmail.com>
2023-02-12 23:02:45 +0100

    FEAT bombs

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const playButton = document.getElementById("play-button");
-const grid = document.getElementById("grid");
+const grid = document.querySelector("#grid");
+const playButton = document.querySelector("#play");
 
-playButton.addEventListener("click", generateGrid);
-
-function generateGrid() {
-  for (let i = 1; i <= 100; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.textContent = i;
-    square.addEventListener("click", function() {
-      console.log("Casella cliccata: " + i);
-      square.classList.add("clicked");
-    });
-    grid.appendChild(square);
+playButton.addEventListener("click", function() {
+  let bombs = [];
+  while (bombs.length < 16) {
+    let randomNumber = Math.floor(Math.random() * 100 + 1);
+    if (!bombs.includes(randomNumber)) {
+      bombs.push(randomNumber);
+    }
   }
-}
+
+  for (let i = 1; i <= 100; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.textContent = i;
+    cell.addEventListener("click", function() {
+      if (bombs.includes(i)) {
+        this.classList.add("bomb");
+        alert("Hai perso!");
+      } else {
+        this.classList.add("clicked");
+      }
+    });
+    grid.appendChild(cell);
+  }
+});
